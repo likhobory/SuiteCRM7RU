a0615ce8894e1eb0b091bc7db1bdfe4492a7248e
likhobory <grego@list.ru>
2013-12-18 11:32:58 +0400

    language file

---
+++ rapira-suite_pack_russian-7.0/custom/modules/FP_Event_Locations/Ext/Language/ru_ru.lang.ext.php
@@ -0,0 +1,9 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+
+
+//THIS FILE IS AUTO GENERATED, DO NOT MODIFY
+$mod_strings['LBL_FP_EVENTS_FP_EVENT_LOCATIONS_1_FROM_FP_EVENTS_TITLE'] = 'События';
+$mod_strings['LBL_FP_EVENT_LOCATIONS_FP_EVENTS_1_FROM_FP_EVENTS_TITLE'] = 'События';
+
+?>
