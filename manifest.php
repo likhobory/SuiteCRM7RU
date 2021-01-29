c9470df5ef3ca5e0c72f1c5cb5b510a0f945f611
likhobory <grego@list.ru>
2021-01-29 17:22:09 +0300

    Обновление языковых файлов до версии 7.11.18

--- manifest.php
+++ manifest.php
@@ -15,8 +15,8 @@ $manifest = array(
 	'acceptable_sugar_flavors' =>
 		  array('CE'),
 	'author' => 'likhobory',
-    'version' => '7.11.17',
-	'published_date' => '2020/11/02',
+    'version' => '7.11.18',
+	'published_date' => '2021/01/29',
       );
 
 $installdefs = array(
