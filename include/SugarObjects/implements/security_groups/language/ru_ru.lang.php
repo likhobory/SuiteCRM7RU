0fbea391ffdcdc2597a84c4d95f63c364d799c42
likhobory <likhobory@mail.ru>
2026-01-30 15:10:35 +0300

    Edit headers

--- include/SugarObjects/implements/security_groups/language/ru_ru.lang.php
+++ include/SugarObjects/implements/security_groups/language/ru_ru.lang.php
@@ -1,4 +1,5 @@
 <?php
+if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 /*********************************************************************************
  *
  * This program is free software; you can redistribute it and/or modify it under
